1655a68f5632a496255400bfb264e1f2dcdc946b
Subhash Chandran <subwiz@gmail.com>
2010-09-28 22:47:09 +0000

    Added verbose option and changed messages to formatter format.

--- src/main/java/org/wiztools/xmlvalidator/XmlValidatorMain.java
+++ src/main/java/org/wiztools/xmlvalidator/XmlValidatorMain.java
@@ -22,6 +22,15 @@ public class XmlValidatorMain {
     @Argument(value="h", alias="help")
     boolean isHelp;
 
+    @Argument(value="v", alias="verbose")
+    boolean isVerbose;
+
+    // All the messages that need to be displayed:
+    private static final String msg_compile_success = "Schema Compile Successful: [%1$s]";
+    private static final String msg_compile_fail = "Error compiling schema: %1$s";
+    private static final String msg_validation_success = "Validation Successful: [%1$s] against [%2$s]";
+    private static final String msg_validation_fail = "Error validating [%1$s] against [%2$s]";
+
     public static void main(String[] arg) throws IOException {
         XmlValidatorMain parser = new XmlValidatorMain();
         List<String> extras = Args.parse(parser, arg);
@@ -37,21 +46,32 @@ public class XmlValidatorMain {
             System.exit(1);
         }
 
+        // If there are failures during execution, exit with non-0 code
+        boolean hasFailures = false;
+
         // Compile the schemas:
         Schema wxs = null;
         try{
             wxs = parser.wxsSchema!=null? SchemaUtil.getWXSSchema(new File(parser.wxsSchema)): null;
+            if(wxs != null && parser.isVerbose) {
+                System.out.printf(msg_compile_success, parser.wxsSchema);
+            }
         }
         catch(SAXException ex) {
-            System.err.println("Error compiling schema: " + parser.wxsSchema);
+            hasFailures = true;
+            System.err.printf(msg_compile_fail, parser.wxsSchema);
             ex.printStackTrace(System.err);
         }
         Schema rng = null;
         try{
             rng = parser.rngSchema!=null? SchemaUtil.getRNGSchema(new File(parser.rngSchema)): null;
+            if(rng != null && parser.isVerbose) {
+                System.out.printf(msg_compile_success, parser.rngSchema);
+            }
         }
         catch(SAXException ex) {
-            System.err.println("Error compiling schema: " + parser.rngSchema);
+            hasFailures = true;
+            System.err.printf(msg_compile_fail, parser.rngSchema);
             ex.printStackTrace(System.err);
         }
 
@@ -59,18 +79,30 @@ public class XmlValidatorMain {
         for(String f: extras) {
             try{
                 if(wxs != null) SchemaUtil.validate(wxs, new File(f));
+                if(wxs != null && parser.isVerbose) {
+                    System.out.printf(msg_validation_success, f, parser.wxsSchema);
+                }
             }
             catch(SAXException ex) {
-                System.err.println("Error validating [" + f + "] against [" + parser.wxsSchema + "]");
+                hasFailures = true;
+                System.err.printf(msg_validation_fail, f, parser.wxsSchema);
                 ex.printStackTrace(System.err);
             }
             try{
                 if(rng != null) SchemaUtil.validate(rng, new File(f));
+                if(rng != null && parser.isVerbose) {
+                    System.out.printf(msg_validation_success, f, parser.rngSchema);
+                }
             }
             catch(SAXException ex) {
-                System.err.println("Error validating [" + f + "] against [" + parser.rngSchema + "]");
+                hasFailures = true;
+                System.err.printf(msg_validation_fail, f, parser.rngSchema);
                 ex.printStackTrace(System.err);
             }
+        }
+
+        if(hasFailures) {
+            System.exit(2);
         }
      }
 }
